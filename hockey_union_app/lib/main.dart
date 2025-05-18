b8a377b13a3cb191ddc583307ced410cfa34df8d
katataHelmat <133001761+foddyhelmut@users.noreply.github.com>
2025-05-19 01:20:05 +0200

    Update main.dart

--- hockey_union_app/lib/main.dart
+++ hockey_union_app/lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:hockey_union_app/screens/home_screen.dart';
+import 'screens/team_registration_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/event_entries_screen.dart';
+import 'screens/player_management_screen.dart';
+import 'screens/realtime_info_screen.dart';
 
 void main() {
-  runApp(HockeyUnionApp());
+  runApp(const HockeyUnionApp());
 }
 
 class HockeyUnionApp extends StatelessWidget {
+  const HockeyUnionApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Namibia Hockey Union',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeScreen(),
+      title: 'Hockey Union App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+      routes: {
+        '/home': (context) => const HomeScreen(),
+        '/team_registration': (context) => const TeamRegistrationScreen(),
+        '/event_entries': (context) => const EventEntriesScreen(),
+        '/player_management': (context) => const PlayerManagementScreen(),
+        '/realtime_info': (context) => const RealtimeInfoScreen(),
+      },
     );
   }
 }
