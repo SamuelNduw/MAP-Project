308af7bc87534d6073b2afb15807e21f9837e1d2
SamuelNduw <samuelwamankalnduw@gmail.com>
2025-05-26 04:54:27 +0200

    changed the ip back to localhost

--- Frontend/hockeyapp/lib/config.dart
+++ Frontend/hockeyapp/lib/config.dart
@@ -1,3 +1,3 @@
 // Easily switch between IPs or environments here
-const String apiBaseUrl = 'http://192.168.178.41:8000/api/'; // 192.168.178.41 or 10.0.2.2
-const String ipTestUrl = '192.168.178.41';
+const String apiBaseUrl = 'http://10.0.2.2:8000/api/'; // 192.168.178.41 or 10.0.2.2
+const String ipTestUrl = '10.0.2.2';
